cdef6e13becce523fd9fae4850d8e7d7fb54c31e
Alexey Simakov <a.simakov@inventos.ru>
2019-05-15 20:10:36 +0300

    React-admin v2.9.1 compatibility and this package v1.9.1

--- README.md
+++ README.md
@@ -1,26 +1,22 @@
-# aor-datetime-input
+# ra-datetime-input
 
-DateTimeInput component for [Admin-on-Rest](https://github.com/marmelab/admin-on-rest).
-Some tips:
-- TimePicker has not prop `container` and it's always as the dailog(by therminology DatePicker), and so `container='dialog'` for DatePicker is hard-code option.
-- Also a button to clear field is placed to component.
-- For DatePicker and TimePicker was set hard-code style to place its in one line.
+Unofficial DateTimeInput component for [react-admin](https://marmelab.com/react-admin).
 
 Component will work in two steps:
 1) Select date
 
-![date](https://user-images.githubusercontent.com/11376039/32560600-1b00aefe-c4bb-11e7-9375-fda8459d627a.jpg)
+![date](https://user-images.githubusercontent.com/11376039/57787903-debb3800-773e-11e9-8fe5-dd3a37c4ed51.jpg)
 
 2) Select time
 
-![time](https://user-images.githubusercontent.com/11376039/32560601-1b1dba8a-c4bb-11e7-958a-25f60c457a31.jpg)
+![time](https://user-images.githubusercontent.com/11376039/57787901-debb3800-773e-11e9-9979-f244b073dd1e.jpg)
 
 Just try it!
 
 ## Installation
 
 ```
-npm install aor-datetime-input --save
+npm install ra-datetime-input --save
 ```
 
 
@@ -35,8 +31,8 @@ import {
     LongTextInput,
     TabbedForm,
     FormTab,
-} from 'admin-on-rest'
-import DateTimeInput from 'aor-datetime-input';
+} from 'react-admin'
+import DateTimeInput from 'ra-datetime-input';
 
 export const NewsEdit = (props) => (
   <Edit title={<NewsTitle />} {...props}>
@@ -60,7 +56,7 @@ npm run build
 
 ## License
 
-This library is licensed under the [MIT Licence](https://github.com/iamsimakov/aor-datetime-input/blob/master/LICENSE).
+This library is licensed under the [MIT Licence](https://github.com/iamsimakov/ra-datetime-input/blob/master/LICENSE).
 
-Copyright (c) 2017 iamsimakov.
 
+Copyright (c) 2019 iamsimakov.
--- src/index.js
+++ src/index.js
@@ -1,133 +1,83 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
-import DatePicker from 'material-ui/DatePicker';
-import TimePicker from 'material-ui/TimePicker';
-import BackspaceIcon from 'material-ui/svg-icons/content/backspace';
-import IconButton from 'material-ui/IconButton';
-import {FieldTitle} from 'admin-on-rest';
+import { addField, FieldTitle } from 'ra-core';
+import { DateTimePicker, MuiPickersUtilsProvider } from 'material-ui-pickers';
+import DateFnsUtils from '@date-io/date-fns';
 
-export const datify = input => {
-    if (!input) {
-        return null;
-    }
-
-    const date = input instanceof Date ? input : new Date(input);
-    if (isNaN(date)) {
-        throw new Error(`Invalid date: ${input}`);
-    }
-
-    return date;
+const datify = input_date => {
+  if (!input_date || input_date === '') {
+    return null;
+  }
+  const date = input_date instanceof Date ? input_date : new Date(input_date);
+  if (isNaN(date)) {
+    throw new Error(`Invalid date: ${input_date}`);
+  }
+  return date;
 };
 
-class DateTimeInput extends Component {
-    
-    onChange = (_, date) => {
-        if (this.props.input.value) {
-            let tempDate = new Date(this.props.input.value);
-            date.setHours(tempDate.getHours());
-            date.setMinutes(tempDate.getMinutes());
-            date.setSeconds(tempDate.getSeconds());
-        }
-
-        this.props.input.onChange(date);
-        this.props.input.onBlur();
-        this.refs[`${this.props.source}.timePicker`].openDialog();
+export class DateTimeInput extends Component {
+    onChange = date => {
+      this.props.input.onChange(date);
     };
-
-    onChangeTime = (_, time) => {
-        this.props.input.onChange(time);
-        this.props.input.onBlur();
-    };
-
-    clearDate = () => {
-        this.props.input.onChange(null);
-        this.props.input.onBlur();
-    };
-
-    /**
-     * This aims to fix a bug created by the conjunction of
-     * redux-form, which expects onBlur to be triggered after onChange, and
-     * material-ui, which triggers onBlur on <DatePicker> when the user clicks
-     * on the input to bring the focus on the calendar rather than the input.
-     *
-     * @see https://github.com/erikras/redux-form/issues/1218#issuecomment-229072652
-     */
-    onBlur = () => {};
-
-    onDismiss = () => this.props.input.onBlur();
 
     render() {
-        // elStyle deleted because timepicker has not container prop and always show as dialog. for same showing date and time pickers.
-        const {
-            input,
-            isRequired,
-            label,
-            meta: { touched, error },
-            options,
-            optionsTime,
-            source,
-            resource,
-            labelTime,
-            timeFormat
-        } = this.props;
-
+      const {
+          className,
+          meta,
+          input,
+          isRequired,
+          label,
+          options,
+          source,
+          resource,
+          ...rest
+      } = this.props;
+      
+      if (typeof meta === 'undefined') {
+        throw new Error(
+          "The DateTimeInput component wasn't called within a redux-form <Field>. Did you decorate it and forget to add the addField prop to your component? See https://marmelab.com/react-admin/Inputs.html#writing-your-own-input-component for details."
+        );
+      }
+      const { touched, error } = meta;
+        
         return (
-          <div>
-            <DatePicker
-            {...input}
-            errorText={touched && error}
-            floatingLabelText={<FieldTitle label={label} source={source} resource={resource} isRequired={isRequired} />}
-            DateTimeFormat={Intl.DateTimeFormat}
-            mode='portrait'
-            container='dialog'
-            autoOk
-            value={datify(input.value)}
-            onChange={this.onChange}
-            onBlur={this.onBlur}
-            onDismiss={this.onDismiss}
-            style={{display: 'inline-block', marginRight: '10px'}}
-            ref={`${this.props.source}.datePicker`}
-            {...options} />
-
-            <TimePicker
-            {...input}
-            errorText={touched && error}
-            floatingLabelText={<FieldTitle label={labelTime ? labelTime : 'Time(hours, mins.)'} source={source} resource={resource} isRequired={isRequired} />}
-            format={timeFormat ? timeFormat : '24hr'}
-            autoOk
-            value={datify(input.value)}
-            onChange={this.onChangeTime}
-            onBlur={this.onBlur}
-            onDismiss={this.onDismiss}
-            style={{display: 'inline-block'}}
-            ref={`${this.props.source}.timePicker`}
-            {...optionsTime} />
-
-            <IconButton onClick={this.clearDate} tooltip="Clear Date" tooltipPosition="top-right">
-              <BackspaceIcon color='grey' hoverColor='black'/>
-            </IconButton>
+          <div className="picker">
+            <MuiPickersUtilsProvider utils={DateFnsUtils}>
+              <DateTimePicker
+                className={className}
+                error={!!(touched && error)}
+                ampm={false}
+                helperText={touched && error}
+                value={datify(input.value)}
+                onChange={this.onChange}
+                label={<FieldTitle
+                  label={label}
+                  source={source}
+                  resource={resource}
+                  isRequired={isRequired}
+                />}
+                clearable
+                {...options}/>
+            </MuiPickersUtilsProvider>
           </div>
         );
     }
 }
 
 DateTimeInput.propTypes = {
-    addField: PropTypes.bool.isRequired,
-    elStyle: PropTypes.object,
-    input: PropTypes.object,
-    isRequired: PropTypes.bool,
-    label: PropTypes.string,
-    meta: PropTypes.object,
-    options: PropTypes.object,
-    resource: PropTypes.string,
-    source: PropTypes.string,
-    labelTime: PropTypes.string
+  classes: PropTypes.object,
+  className: PropTypes.string,
+  input: PropTypes.object,
+  isRequired: PropTypes.bool,
+  label: PropTypes.string,
+  meta: PropTypes.object,
+  options: PropTypes.object,
+  resource: PropTypes.string,
+  source: PropTypes.string,
 };
 
 DateTimeInput.defaultProps = {
-    addField: true,
-    options: {},
+  options: {},
 };
 
-export default DateTimeInput;
-
+export default addField(DateTimeInput);
